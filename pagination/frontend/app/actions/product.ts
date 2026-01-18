6a71f3fe734da95525442f060c0877c7c82182ea
IzharMohammed <izharmohammed21@gmail.com>
2026-01-18 19:26:07 +0530

    Add pagination

---
+++ pagination/frontend/app/actions/product.ts
@@ -0,0 +1,26 @@
+const BACKEND_URL = "http://localhost:4000";
+
+export async function getProducts({ page, limit }: { page: string, limit: string }) {
+    try {
+        const params = { page, limit }
+        const url = `${BACKEND_URL}/api/products?` + new URLSearchParams(params);
+        const response = await fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+
+        const result = await response.json();
+        return {
+            status: 200,
+            success: true,
+            result,
+        }
+    } catch (error) {
+        console.error("Error fetching products");
+        throw error;
+    }
+}
